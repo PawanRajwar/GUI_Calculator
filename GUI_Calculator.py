f3e37db848f400a28723706b6079bf93e44949ac
Pawan Singh Rajwar <pawansinghrajwar@gmail.com>
2024-11-06 17:27:23 +0530

    Starting the project to make GUI Calculator

---
+++ GUI_Calculator.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+class CalculatorApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Advanced Calculator")
+        self.root.geometry("400x550")
+        self.root.resizable(False, False)
+        
+
